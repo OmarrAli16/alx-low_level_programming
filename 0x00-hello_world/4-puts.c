843213daec0a3e79569deaba80ee4df8facf215f
OmarrAli16 <omar123ali2000@gmail.com>
2023-09-07 13:29:45 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
 int main(void)
 {
-	puts ("Programming is like building a multilingual puzzle");
-
+	puts("Programming is like building a multilingual puzzle");
+/***
+ * this program for displaying
+ * **/
 		return (0);
 }
+
