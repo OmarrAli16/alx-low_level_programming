a250f38b0de4aba5369593dfec59046d3cb5e9cf
OmarrAli16 <omar123ali2000@gmail.com>
2023-09-07 13:34:42 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
+/**
+ * main - entry point
+ * return : always 0 (success
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
-/***
- * this program for displaying
- * **/
 		return (0);
 }
 
