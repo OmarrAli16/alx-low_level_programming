889b26c4b60a68cfad59a4c613ef3869fcce97ad
OmarrAli16 <omar123ali2000@gmail.com>
2023-09-08 08:57:20 -0700

    alphrev

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print z-a
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	char last = 'z';
+
+	while (last >= a)
+	{
+		putchar(last);
+		last--;
+	}
+	putchar('\n');
+
+	return (0);
+}
