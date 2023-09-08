375f5cb5723c232b125dc61c2adc513b9aa82d3a
OmarrAli16 <omar123ali2000@gmail.com>
2023-09-08 08:42:05 -0700

    digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print all single digit numbers
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int num = 0;
+
+
+	while (num < 10)
+	{
+		printf("%i",num);
+		num++;
+	}
+	printf("\n")
+
+	return (0);
+}
