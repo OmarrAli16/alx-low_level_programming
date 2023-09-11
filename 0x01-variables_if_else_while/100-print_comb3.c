9e7a56e66698f5095c247cf484658c016147e393
OmarrAli16 <omar123ali2000@gmail.com>
2023-09-11 11:56:51 -0700

    comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,27 +11,27 @@
 
 int main(void)
 {
-	int num1 = '0';
-	int num2 = '0';
+	int num1 = 0;
+	int num2;
 
-	while (num1 <= '8')
+	while (num1 <= 8)
 	{
-		while (num2 <= '9')
+		num2 = 0;
+		while (num2 <= 9)
 		{
-			if (num1 != num2)
+			if (num1 < num2 && num1 != num2)
 			{
-				putchar(num1);
-				putchar(num2);
-				if (num1 != '8' && num2 != '9')
+				putchar(num1 + '0');
+				putchar(num2 + '0');
+				if (num1 + num2 != 17)
 				{
-					putchar(' ');
 					putchar(',');
+					putchar(' ');
 				}
 			}
-		num2++;
+			num2++;
 		}
-	num1++;
-	num2 = num1 + 1;
+		num1++;
 	}
 	putchar('\n');
 	return (0);
