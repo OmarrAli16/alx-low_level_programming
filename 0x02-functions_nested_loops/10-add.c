0c8c93aaf4d0eda04c124137ce1112cba7201f7f
OmarrAli16 <omar123ali2000@gmail.com>
2023-09-12 22:39:49 -0700

    put

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,8 +3,8 @@
 
 /**
  * add - adds two integers
- * num1: The first integer to be added
- * num2: The second integer to be added
+ * @num1: The first integer to be added
+ * @num2: The second integer to be added
  * Return: the result of the sum of two numbers
  */
 int add(int num1, int num2)
