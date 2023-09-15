c3f0dc449d1ae5a3f0088d1070409fb9e5cb01fd
OmarrAli16 <omar123ali2000@gmail.com>
2023-09-14 19:45:19 -0700

    diag

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagonal - a function that draws a diagonal line
+ *
+ * @n: input integer for count '\'
+ */
+
+void print_diagonal(int n)
+{
+	int diag;
+	int space;
+
+	if (n < 0)
+		_putchar('\n');
+	else
+	{
+		for (diag = 1; diag <= n; diag++)
+		{
+			for (space = 1; space <= diag; space++)
+				_putchar(' ');
+			_putchar('/');
+			putchar('\n');
+		}
+	}
+}
