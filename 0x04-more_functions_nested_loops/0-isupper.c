24837cea8fc7853fbf648c1ae6a1bfef9527856a
OmarrAli16 <omar123ali2000@gmail.com>
2023-09-14 18:16:07 -0700

    upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isupper - is a function that checks for uppercase character
+ *
+ * @c: input for chara
+ *
+ * Return: 1 if it is uppecase
+ */
+
+int _isupper(int c)
+{
+	if (c <= 'z' && c >= 'a')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef  MAIN_h
+#define MAIN_h
+
+#include <stdio.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+int _isupper(int c);
+
+#endif
