69fc7dc7fcba623f5f8a805977b3ce43395a6daa
OmarrAli16 <omar123ali2000@gmail.com>
2023-09-18 15:05:11 -0700

    98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - a function that takes a points and give it value 98
+ *
+ * @n: integer inserted
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
