09b1fe6ffaeea5d0febfe834701a5119de47000d
OmarrAli16 <omar123ali2000@gmail.com>
2023-09-20 23:05:12 -0700

    ncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,8 +18,8 @@ char *_strncat(char *dest, char *src, int n)
 	int length;
 	int i;
 
-	for (length = 0; dest[length]; length++)
-		;
+	while (dest[length])
+		length++;
 	for (i = 0; src[i] != '\0' && i < n; i++)
 	{
 		dest[length + i] = src[i];
