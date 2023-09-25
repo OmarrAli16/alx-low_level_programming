b4b55dd43af59b6f05b11a3535dbdce876aebfef
OmarrAli16 <omar123ali2000@gmail.com>
2023-09-25 16:16:12 -0700

    chess

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -4,11 +4,24 @@
 /**
  * print_chessboard - a function that prints the chessboard
  *
- * @a:
- * */
+ * @a: number of rows
+ */
 
 void print_chessboard(char (*a)[8])
 {
+	int i;
+	int j;
 
-
+	i = 0;
+	j = 0;
+	while (i < 8)
+	{
+		while (j < 8)
+		{
+			_putchar(a[i][j]);
+			j++;
+		}
+		i++;
+		_putchar('\n');
+	}
 }
