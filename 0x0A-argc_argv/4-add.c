9667085e4a934b4129031d4897d661e28990c5bd
OmarrAli16 <omar123ali2000@gmail.com>
2023-10-02 17:04:36 -0700

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * @argc:
+ *
+ * @argv:
+ *
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int addition;
+	int numb;
+	int i;
+
+	i = 1;
+	addition = 0;
+	numb = 0;
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	while (i < argc)
+	{
+		i++;
+		numb = atoi(argv[i]);
+			if (atoi(argv[i]) != numb || numb <= 0)
+				{
+				fprintf(stderr, "Error\n");
+				return (1);
+				}
+				addition = addition + numb;
+				}
+				printf("%d\n", addition);
+				return (0);
+				}
