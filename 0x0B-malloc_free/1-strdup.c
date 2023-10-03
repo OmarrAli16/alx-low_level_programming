66e96655331e18dafe482d10c3d740f6e61388eb
OmarrAli16 <omar123ali2000@gmail.com>
2023-10-03 10:36:55 -0700

    dupli

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *_strdup - a function that returns a pointed to a new memory
+ *
+ * @str: inputed string
+ *
+ * Return: a pointer to dublicate string
+ */
+
+char *_strdup(char *str)
+{
+	int cpy;
+	int req;
+	char *result;
+
+	if (str == 0)
+	{
+		return (NULL);
+	}
+	for (req = 0; str[req] != '\0'; req++)
+	{
+		;
+	}
+	result = malloc(req * sizeof(*str) + 1);
+	for (cpy = 0; cpy < req; cpy++)
+	{
+		result[cpy] = str[cpy];
+	}
+	return (result);
+}
