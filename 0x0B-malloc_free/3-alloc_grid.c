e57cb2c7807caaec51efb766f6a4c1d17bd4ebf5
OmarrAli16 <omar123ali2000@gmail.com>
2023-10-04 09:56:34 -0700

    grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,4 +14,34 @@
 
 int **alloc_grid(int width, int height)
 {
+	int i = 0;
+	int j = 0;
+	int **matr;
 
+	matr = (int **)malloc(sizeof(*matr) * height)
+	if (height <= 0 || width <= 0 || matr == 0)
+	{
+		return (0);
+	}
+	else if
+	{
+		for (; i < height; i++)
+		{
+			matr[i] = (int *)malloc(sizeof(**matr) * width);
+			if (matr[i] == 0)
+			{
+				while (i--)
+				{
+					free(matr[i]);
+				}
+				free(matr);
+				return (0);
+			}
+			for (; j < width; j++)
+			{
+				matr[i][j] = 0;
+			}
+		}
+	}
+	return (matr);
+}
