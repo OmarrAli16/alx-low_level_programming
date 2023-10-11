1a466a5f9b159ea8f88f1287cc453cf0312f9581
OmarrAli16 <omar123ali2000@gmail.com>
2023-10-12 02:33:14 +0300

    100

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -19,9 +19,9 @@ int main(int argc, char **argv)
 	if (argc != 2)
 	{
 		printf("Error\n");
-		exit (1);
+		exit(1);
 	}
-	cs = atoi (argv[1]);
+	cs = atoi(argv[1]);
 	if (cs < 0)
 	{
 		printf("Error\n");
