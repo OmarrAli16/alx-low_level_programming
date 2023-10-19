18d6d74122fcccd8437027e68e13851f4e77dfb5
OmarrAli16 <omar123ali2000@gmail.com>
2023-10-19 23:29:22 +0300

    print

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_listint - a function for printing elements
+ *
+ * @h:a pointer to the list that will be printed single linked
+ *
+ * Return: the list size
+ */
+size_t print_list(const list_t *h)
+{
+	size_t num_node;
+
+	num_node = 0;
+	while (h != NULL)
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		num_node++;
+	}
+	return (num_node);
+}
